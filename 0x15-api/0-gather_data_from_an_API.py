8625fec8b524f28769c5477b1965a01e6a30cb0e
VivianDee <vdagbue@gmail.com>
2023-10-23 09:42:41 -0700

    Fix: Synthax errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,19 +3,18 @@
     This script uses REST API nd returns information about
     an employee's todo list
 """
-import sys
 import requests
+import sys
 
 
 def fetch_data(e_id=None):
     """Fetches the data from the url"""
     user_data = requests.get(
-            "https://jsonplaceholder.typicode.com/users/?id={}".format(e_id),
-            verify=False)
+            "https://jsonplaceholder.typicode.com/users/{}".format(e_id))
 
     user_todos = requests.get(
-            "https://jsonplaceholder.typicode.com/todos/?userId={}".format(
-                e_id), verify=False)
+            "https://jsonplaceholder.typicode.com/todos",
+            params={"userId": e_id})
 
     user_data = user_data.json()
     user_todos = user_todos.json()
@@ -25,7 +24,6 @@ def fetch_data(e_id=None):
 
 def display_progress(data, todo):
     """Displays the employee's TODO list progress"""
-    user_name = data[0].get("name")
     completed_tasks = 0
     total_tasks = 0
 
@@ -35,10 +33,10 @@ def display_progress(data, todo):
         total_tasks += 1
 
     print("Employee {} is done with tasks({}/{})".format(
-        user_name, completed_tasks, total_tasks))
+        data.get("name"), completed_tasks, total_tasks))
     for task in todo:
         if task.get("completed"):
-            print("\t{}".format(task.get("title")))
+            print("\t {}".format(task.get("title")))
 
 
 if __name__ == "__main__":
